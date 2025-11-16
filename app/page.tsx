ca8a0d98604eee371136c0dcad575bd47a56d2e4
MW <m.m.wilczynski@icloud.com>
2025-11-16 14:12:24 +0000

    issue summary page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
 import Image from "next/image";
 import LatestIssues from "./LatestIssues";
+import IssueSummary from "./IssueSummary";
+import prisma from "@/prisma/client";
 
-export default function Home({}) {
-  return <LatestIssues />;
+export default async function Home({}) {
+  const open = await prisma.issue.count({
+    where: { status: "OPEN" },
+  });
+  const inprogress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({
+    where: { status: "DONE" },
+  });
+  return <IssueSummary open={open} inProgress={inprogress} closed={closed} />;
 }
